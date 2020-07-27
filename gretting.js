9a97f9b45a286e5b2a46f04a9b46a377b9745882
DragooCho <66540054+DragooCho@users.noreply.github.com>
2020-07-27 18:27:57 +0900

    add study

--- gretting.js
+++ gretting.js
@@ -5,6 +5,22 @@ const from = document.querySelector(".js-from"),
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  from.classList.add(SHOWING_CN);
+  from.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
   from.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,7 +30,7 @@ function paintGreeting(text) {
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
   if (currentUser === null) {
-    //she is not
+    askForName();
   } else {
     paintGreeting(currentUser);
   }
