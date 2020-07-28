f982cf1247956dda8167c0d24fe1346109b723a7
DragooCho <66540054+DragooCho@users.noreply.github.com>
2020-07-28 21:46:07 +0900

    add todo

--- todo.js
+++ todo.js
@@ -1,27 +1,32 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoForm.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
-//writing no.1
 
 const TODOS_LS = "toDos";
-// writing no.4
-// const TODOS_LS = "toDos"는
-// 단지 문자열을 유지하는 것입니다.
-// "ToDos"는 문자열입니다.
-// ToDos는 할일 배열을 보유하는 변수입니다.
+
+const toDos = [];
+
+function saveTodos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 
 function paintToDo(text) {
   const li = document.createElement("li");
-  //writing no.4
-  //document.createElement는 현 html에 없는 테그,클래스를 생성해준다.
-  //button의 영역을 설정해주고 그 영역에 이모지를 삽입하는 과정을 보여준다.
   const delbtn = document.createElement("button");
-  delbtn.innerHTML = "❌";
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
+  delbtn.innerText = "❌";
   span.innerText = text;
   li.appendChild(delbtn);
   li.appendChild(span);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId,
+  };
+  toDos.push(toDoObj);
+  saveTodos();
 }
 
 function handleSubmit(event) {
@@ -42,12 +47,7 @@ function loadToDos() {
 function init() {
   loadToDos();
   toDoForm.addEventListener("submit", handleSubmit);
-  // writing no.5
 }
 
 init();
-
-// init();
-// function init() {
-//     loadToDos();
-// writing no.2
+// xfbdxbdfxbdxbn
